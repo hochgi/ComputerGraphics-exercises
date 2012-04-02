cc35be53a6f763dc6ae59f87e1de21ef78cd22e3
Gilad Hoch <giladhoch@yahoo.com>
2012-04-02 13:21:30 +0300

    ex1 folder changes

---
+++ CG_ex1/src/view_module/CGexercise1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package view_module;
+
+/**
+ * @author giladhoch
+ *
+ */
+public class CGexercise1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
